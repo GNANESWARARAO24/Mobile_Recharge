043deeddbfc532b4788923953e6bf8500a735b66
Johnny <gnaneswararaobheemuni@gmail.com>
2025-08-01 17:03:35 +0530

    updated

--- mobile_recharge_frontend/server.js
+++ mobile_recharge_frontend/server.js
@@ -5,20 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // ✅ Replace the folder name with your actual build output folder
-const distPath = path.join(__dirname, "dist/mobile-recharge-frontend");
+const distPath = path.join(__dirname, "dist/mobile_recharge_frontend");
 
 // ✅ Serve static Angular files
 app.use(express.static(distPath));
 
 // ✅ Redirect all routes to index.html (SPA fallback)
 app.get("*", (req, res) => {
-<<<<<<< HEAD
   res.sendFile(path.join(distPath, "index.html"));
-=======
   res.sendFile(
     path.join(__dirname, "dist/mobile_recharge_frontend/browser/index.html")
   );
->>>>>>> e8d7954736a599a2f0aab1752409950c67b2be22
 });
 
 app.listen(PORT, () => {
